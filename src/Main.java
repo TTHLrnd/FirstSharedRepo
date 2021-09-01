4e644b7880fa88c6f557897c881a737a12705910
TTHLrnd <lorandtoth.lori@gmail.com>
2021-09-01 19:58:38 +0200

    no message

--- src/Main.java
+++ src/Main.java
@@ -70,5 +70,14 @@ public class Main {
         return sum;
     }
 
+    public static int sumOfEvens(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0)
+                sum+= array[i];
+        }
+        return sum;
+    }
+
 
 }
