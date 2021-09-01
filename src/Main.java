a7b2f1681744169153b614cd9cac77d1928ba98a
TTHLrnd <lorandtoth.lori@gmail.com>
2021-09-01 18:56:57 +0200

    add methods

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,25 @@ public class Main {
             array[i] = random.nextInt(100);
         }
     }
+
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int sumOfEvens(int[] array) {
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] % 2 == 0 ) {
+                sum+= array[i];
+            }
+        }
+        return sum;
+    }
 }
