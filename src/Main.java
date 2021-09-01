13418f4de635da3257e2b128311e4bd178fa1255
Beus06 <kerteszbeata9@gmail.com>
2021-09-01 19:11:27 +0200

    add two methods

--- src/Main.java
+++ src/Main.java
@@ -30,4 +30,24 @@ public class Main {
         }
         return sum;
     }
+
+    public static int findMax ( int[] array) {
+        int max = array[0];
+        for ( int i = 0; i<array.length; i++){
+            if(array[i]>max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int sumOfUnpara (int[] array){
+        int sum = 0;
+        for( int i = 0; i<array.length; i++){
+            if(array[i] % 2 != 0) {
+                sum+= array[i];
+            }
+        }
+        return sum;
+    }
 }
