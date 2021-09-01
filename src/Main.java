20397722059d24e3295baea7685468e7146a585f
TTHLrnd <lorandtoth.lori@gmail.com>
2021-09-01 18:50:59 +0200

    Add array with random nums

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+    }
+}
